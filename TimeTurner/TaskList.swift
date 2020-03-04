ced74070bbca87b55d9f07fccadc70d5257f0649
Kun Jeong <kunjeong99@naver.com>
2020-03-05 00:12:30 +0900

    fixed #7

--- TimeTurner/TaskList.swift
+++ TimeTurner/TaskList.swift
@@ -34,7 +34,10 @@ struct TaskList: View {
     var body: some View {
         VStack {
             HStack{
-                TextField("Task Name", text: $taskName)
+                TextField("Task Name", text: $taskName){
+                    self.addTask()
+                    
+                }
                     .textFieldStyle(PlainTextFieldStyle())
                 Button(action: {
                     self.addTask()
@@ -64,6 +67,7 @@ struct TaskList: View {
         newTask.isComplete = false
         newTask.name = taskName
         newTask.dateAdded = Date()
+        taskName = ""
         do {
             try context.save()
         } catch {
