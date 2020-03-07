a64162bdca74f8e2c20165782919f4b16db124b5
Kun Jeong <kunjeong99@naver.com>
2020-03-07 13:47:38 +0900

    Added iOS target

---
+++ TimeTurner_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeTurner_iOS
+//
+//  Created by Kun Jeong on 2020/03/07.
+//  Copyright © 2020 Kun Jeong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
